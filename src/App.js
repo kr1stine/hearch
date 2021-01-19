e7057fa900f68dcee6872f5c36f5bec0ce5d81cf
Kristine Leetberg <kr1stine@ut.ee>
2021-01-19 15:53:53 +0200

    Initial axios test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getSubtitles } from 'youtube-captions-scraper';
 
 function App() {
+  const KEY = "AIzaSyDSAQ3Z7PJvOJs0B28kyjkuwc88_IY5_Ac";
+  const req = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+      part: 'snippet',
+      maxResults: 5,
+      key: KEY
+    }
+  })
+  const [captions, setCaptions] = useState([]);
+
+  useEffect(() => {
+    getSubtitles({
+      videoID: 'B6edjW4ewYo', // youtube video id
+      lang: 'en' // default: `en`
+    }).then(resp => {
+      console.log(resp);
+      setCaptions(resp)
+    });
+  }, []); 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {captions}
         </p>
         <a
           className="App-link"
---
+++ src/mock-data.js
@@ -0,0 +1,32 @@
+/* req.get('/captions', {
+     params: {
+        part: 'snippet',
+        videoId: 'JxSVP1NqBCU'
+      }
+    }) */
+export const captionList = {
+    "kind": "youtube#captionListResponse",
+    "etag": "a5bVeL0zffg6Dcp1twxB7xVJO-k",
+    "items": [
+      {
+        "kind": "youtube#caption",
+        "etag": "k093o7tGm6UvU7xWFCtaJUKKbLQ",
+        "id": "F1sk24w1UrZJC_ANwnwgaUIaoQwVHkPIjEKMoCTDoBw=",
+        "snippet": {
+          "videoId": "JxSVP1NqBCU",
+          "lastUpdated": "2021-01-17T00:51:19.092174Z",
+          "trackKind": "asr",
+          "language": "en",
+          "name": "",
+          "audioTrackType": "unknown",
+          "isCC": false,
+          "isLarge": false,
+          "isEasyReader": false,
+          "isDraft": false,
+          "isAutoSynced": false,
+          "status": "serving"
+        }
+      }
+    ]
+  }
+  
